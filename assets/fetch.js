48e1d97624b65f6b88964d4dee56ff4daa07f1eb
Molly Durst <mollymadegraphics@yahoo.com>
2020-11-25 21:07:40 -0500

    cleaned up html got js ready for local storage

--- assets/fetch.js
+++ assets/fetch.js
@@ -1,15 +1,10 @@
 // JavaScript Document
 
 //API for Taco
-<<<<<<< HEAD
 // http://taco-randomizer.herokuapp.com/random/?full-taco=true
 
 document.getElementById('buttonCombo').onclick = function getTacoAndBeer() {
 	
-=======
-http://taco-randomizer.herokuapp.com/random/?full-taco=true
-
->>>>>>> ca49ea963a8db272b06059f381be544e0b1eb753
 //call fetch
 	var api_url = "http://taco-randomizer.herokuapp.com/random/?full-taco=true";
 	async function getTaco (){
@@ -18,25 +13,17 @@ http://taco-randomizer.herokuapp.com/random/?full-taco=true
 //complete data stream		
 	const data = await response.json();
 	const {tacoName} = data;
-<<<<<<< HEAD
 	console.log(data.base_layer.name)	
-=======
->>>>>>> ca49ea963a8db272b06059f381be544e0b1eb753
 //display in browser as text		
 	document.getElementById("viewTaco").innerHTML = ("Taco: " + data.base_layer.name);	
 	}
 
 getTaco();
 
-<<<<<<< HEAD
 
 
 //API for Beer
 // http://https://api.punkapi.com/v2/beers/random
-=======
-//API for Beer
-http://https://api.punkapi.com/v2/beers/random
->>>>>>> ca49ea963a8db272b06059f381be544e0b1eb753
 
 //call fetch
 fetch("https://api.punkapi.com/v2/beers/random")
@@ -48,15 +35,12 @@ fetch("https://api.punkapi.com/v2/beers/random")
 	//display in browser as text
 	document.getElementById("viewBeer").innerHTML = ("Beer: " + beer);
 })
-<<<<<<< HEAD
 	
 	}
 
 //Save Favorites
 localStorage.getItem("beer", "data.base_layer.name");
 
-=======
->>>>>>> ca49ea963a8db272b06059f381be544e0b1eb753
 
 
 
